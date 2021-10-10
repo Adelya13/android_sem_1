ebd7b5ebaa0bc8070e0c685e3a2aa10d68cb81bc
Adelya <valisheva2002@mail.ruclear>
2021-10-10 11:42:56 +0300

    add binding

--- app/src/main/java/kpfu/itis/valisheva/app1/MainActivity.kt
+++ app/src/main/java/kpfu/itis/valisheva/app1/MainActivity.kt
@@ -2,10 +2,17 @@ package kpfu.itis.valisheva.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kpfu.itis.valisheva.app1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also{
+            setContentView(it.root)
+        }
+
     }
 }
